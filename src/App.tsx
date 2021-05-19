867ae4eaab22ac9fd087d94d73d10cce05eee00a
kaylawoodbury <kaylasandoval92@gmail.com>
2021-05-19 16:12:28 +0200

    started styling landing page
    Co-authored-by: jaime <jaime.cruz.hostalot@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import landscape from "./images/image1.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={styles.background}>
+      <div style={styles.container}>
+        <p style={styles.title}>Welcome to KidzMathicon</p>
+      </div>
+      <button>Start</button>
+      <a href="https://www.vecteezy.com/free-vector/nature">
+        Nature Vectors by Vecteezy
+      </a>
     </div>
   );
 }
 
 export default App;
+const styles = {
+  background: {
+    backgroundImage: `url(${landscape})`,
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    width: "100vw",
+    height: "100vh",
+  },
+  title: {
+    color: "#FAFF02",
+    fontSize: 55,
+    fontFamily: "Chalkduster",
+  },
+  container: {
+    backgroundColor: "#f8d5e2",
+  },
+};
+
+// <a href="https://www.vecteezy.com/free-vector/nature">Nature Vectors by Vecteezy</a>
+// <a href="https://www.vecteezy.com/free-vector/landscape">Landscape Vectors by Vecteezy</a>
+// <a href="https://www.vecteezy.com/free-vector/nature">Nature Vectors by Vecteezy</a>yarn
