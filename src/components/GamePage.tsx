ca8a270ce5f447753aaf02d5397bc387623c0002
Jaime <jaime.cruz.hostalot@gmail.com>
2021-06-01 15:35:43 +0200

    [New Package] Added new package chance for random generating integers.
    Co-authored-by: kayla <kaylasandoval92@gmail.com>

--- src/components/GamePage.tsx
+++ src/components/GamePage.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
+let Chance = require('chance')
 
 const GamePage = (value: any) => {
+  let chance = new Chance()
+  let randomMultiplicator = chance.integer({ min: 0, max: 10 })
+
   console.log({value})
   return (
     <div>
-      <p > Hello =)</p>
+      <p >{randomMultiplicator}</p>
     </div>
   )
 }
