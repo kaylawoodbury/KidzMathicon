be459bf10985b62d04dc74215c3bd3d67640e1b9
Jaime <jaime.cruz.hostalot@gmail.com>
2021-06-01 16:23:21 +0200

    [Style, Boxes] Added new styling options in the css file[Box], added Interface gor the numbers as well as styling it.
    Co-authored-by: kayla <kaylasandoval92@gmail.com>

--- src/components/GamePage.tsx
+++ src/components/GamePage.tsx
@@ -4,13 +4,25 @@ let Chance = require('chance')
 const GamePage = (value: any) => {
   let chance = new Chance()
   let randomMultiplicator = chance.integer({ min: 0, max: 10 })
+  let multiplier = value.value
 
   console.log({value})
   return (
-    <div>
-      <p >{randomMultiplicator}</p>
+    <div style={{paddingTop: "10%", display: "flex", alignItems: "center" , justifyContent: "space-between", marginRight: "25vw", marginLeft: "25vw"}}>
+      <p className="Box">{multiplier}</p>
+      <p style={styles.title}>X</p>
+      <p className="Box">{randomMultiplicator}</p>
     </div>
   )
 }
 
 export default GamePage
+
+const styles = {
+  title: {
+    color: "rgb(240,224,42)",
+    fontSize: "10vw",
+    fontFamily: "Permanent Marker",
+    textShadow: "-2px 0 black, 0 2px black, 2px 0 black, 0 -2px black",
+  },
+};
