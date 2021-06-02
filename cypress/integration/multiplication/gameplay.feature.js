144b7419acf2125e004a6f555aa73bc5bc92158d
kaylawoodbury <kaylasandoval92@gmail.com>
2021-06-02 14:53:45 +0200

    testing updatess for stub on gameplay test
    Co-authored-by: jaime <jaime.cruz.hostalot@gmail.com>

--- cypress/integration/multiplication/gameplay.feature.js
+++ cypress/integration/multiplication/gameplay.feature.js
@@ -1,12 +1,14 @@
+const generateNumber = require("../../../src/components/actions/generateNumber")
+
 describe("User can", () => {
   beforeEach(() => {
     cy.visit("/")
-      cy.stub(generateNumber, "generateNumber").as("stub").return(5);
+    cy.get("[data-cy='startButton']").click();
+    cy.get("[data-cy='multiplier-5']").click();
+      cy.stub(generateNumber, "generateNumber").as('randomMultiplier').returns(5);
   });
 
   it("successfully answer the question within the time", () => {
-    cy.get("[data-cy='startButton']").click();
-    cy.get("[data-cy='multiplier-5']").click();
     cy.get("[data-cy='multiplication-input']").type(25);
   });
   // it('successfully answers the question within the time but with incorrect value', () => {
