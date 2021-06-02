b54e8bfdd51e0e685b916b76faf99f1aaa9ffd8a
Jaime <jaime.cruz.hostalot@gmail.com>
2021-06-02 06:55:22 +0200

    [STATE] Added logic to stop multiplicator to change state when input state is being updated.
    Co-authored-by: kayla <kaylasandoval92@gmail.com>

--- src/components/GamePage.tsx
+++ src/components/GamePage.tsx
@@ -1,21 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
 let Chance = require('chance')
 
 const GamePage = (value: any) => {
-  let chance = new Chance()
-  let randomMultiplicator = chance.integer({ min: 0, max: 10 })
+  const [answer, setAnswer] = useState('')
+  const [multiplicator, setMultiplicator] = useState(0)
   let multiplier = value.value
-  let result = multiplier * randomMultiplicator
 
+  if (answer.length < 1 && multiplicator === 0) {
+    let chance = new Chance()
+    let randomNumber = chance.integer({ min: 1, max: 10 })
+    setMultiplicator(randomNumber) 
+  }
+
+  let result = multiplier * multiplicator
 
   console.log({result})
   return (
     <div style={{paddingTop: "10%", display: "flex", alignItems: "center" , justifyContent: "space-between", marginRight: "20vw", marginLeft: "20vw"}}>
       <p className="Box">{multiplier}</p>
       <p style={styles.title}>X</p>
-      <p className="Box">{randomMultiplicator}</p>
+      <p className="Box">{multiplicator}</p>
       <p style={styles.title}>=</p>
-      <input className="Box"></input>
+      <input className="Box" onChange={i => setAnswer(i.target.value)}></input>
     </div>
   )
 }
